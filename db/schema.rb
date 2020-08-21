5ece5b48048fdf3d3a159fe2bd12af137afdfa21
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-08-21 13:32:11 +0900

    devise_token_auth導入後、金子メンターに指摘された箇所修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2020_08_21_032306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "test_users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
