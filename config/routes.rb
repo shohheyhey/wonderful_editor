8a5badb29db2ecd1853a93bb23fe97429e634633
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-08-23 10:42:17 +0900

    task7_2まで。各requests/rspec.rb とroutesを修正。routeにない状態ではrequestのテストができないらしい。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # resources :article_likes
+  # resources :comments
+  # resources :articles
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
