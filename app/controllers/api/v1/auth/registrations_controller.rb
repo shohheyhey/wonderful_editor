c47026aa7165f6bd04e2492afd560c5219248a14
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-08-26 08:04:07 +0900

    task9_1まで postmanでのリクエスト処理がうまくいかない。

---
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  def new
+    user = User.new
+    user.create!(sign_up_params)
+    render json: user
+  end
+
+  private
+
+    def sign_up_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+      params.permit(:name, :email)
+    end
+end
